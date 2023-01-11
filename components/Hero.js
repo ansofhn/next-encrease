c9ab2cd619e8881af4558e3e41ebef31bdd4d7f1
Ansof Habibunnadjar <ansofhn5@gmail.com>
2023-01-11 14:03:54 +0700

    fix: hero height responsive all device

--- components/Hero.js
+++ components/Hero.js
@@ -14,21 +14,19 @@ const Hero = () => {
         spaceBetween={10}
         slidesPerView={1}
         pagination={{ clickable: true }}
-        // onSwiper={(swiper) => console.log(swiper)}
-        // onSlideChange={() => console.log("slide change")}
         speed={1000}
         loop
       >
         <SwiperSlide className="w-full h-full">
-          <div class="py-24 overflow-hidden md:py-32 md:px-10 lg:py-32 lg:pl-32 lg:pr-24 xl:py-0 xl:px-0 2xl:pr-0 xl:ml-10">
+          <div class="py-24 overflow-hidden md:pt-32 md:pb-20 md:px-10 lg:pb-16 lg:pl-32 lg:pr-24 xl:pt-38 xl:px-0 2xl:pt-48 2xl:pb-28 2xl:pr-0 xl:ml-10">
             <div
-              class="w-full h-full px-4 py-40 bg-center bg-no-repeat md:p-10 lg:p-0 2xl:scale-105"
+              class="w-full h-full flex justify-center lg:justify-start py-44 bg-center bg-no-repeat md:p-10 lg:p-0 2xl:scale-105"
               style={{
                 backgroundImage:
                   "url(https://source.unsplash.com/random/1024x620?laptop)",
               }}
             >
-              <div class="md:ml-20 lg:py-32 lg:-ml-20 xl:-ml-0 xl:pl-20 xl:py-72 2xl:pl-48">
+              <div class="lg:py-32 lg:-ml-20 xl:-ml-0 xl:pl-20 xl:py-40 2xl:pl-48">
                 <div class="w-[345px] h-[238px] md:w-[470px] md:h-[325px] bg-gray-200 p-6 md:p-10">
                   <img
                     src="https://source.unsplash.com/random/409x257?touchscreen"
@@ -40,15 +38,15 @@ const Hero = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div class="py-24 overflow-hidden md:py-32 md:px-10 lg:py-32 lg:pl-32 lg:pr-24 xl:py-0 xl:px-0 2xl:pr-0 xl:ml-10">
+          <div class="py-24 overflow-hidden md:pt-32 md:pb-20 md:px-10 lg:pb-16 lg:pl-32 lg:pr-24 xl:pt-38 xl:px-0 2xl:pt-48 2xl:pb-28 2xl:pr-0 xl:ml-10">
             <div
-              class="w-full h-full px-4 py-40 bg-center bg-no-repeat md:p-10 lg:p-0 2xl:scale-105"
+              class="w-full h-full flex justify-center lg:justify-start py-44 bg-center bg-no-repeat md:p-10 lg:p-0 2xl:scale-105"
               style={{
                 backgroundImage:
                   "url(https://source.unsplash.com/random/1024x620?pc)",
               }}
             >
-              <div class="md:ml-20 lg:py-32 lg:-ml-20 xl:-ml-0 xl:pl-20 xl:py-72 2xl:pl-48">
+              <div class="lg:py-32 lg:-ml-20 xl:-ml-0 xl:pl-20 xl:py-40 2xl:pl-48">
                 <div class="w-[345px] h-[238px] md:w-[470px] md:h-[325px] bg-gray-200 p-6 md:p-10">
                   <img
                     src="https://source.unsplash.com/random/409x257?monitor"
@@ -60,15 +58,15 @@ const Hero = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div class="py-24 overflow-hidden md:py-32 md:px-10 lg:py-32 lg:pl-32 lg:pr-24 xl:py-0 xl:px-0 2xl:pr-0 xl:ml-10">
+          <div class="py-24 overflow-hidden md:pt-32 md:pb-20 md:px-10 lg:pb-16 lg:pl-32 lg:pr-24 xl:pt-38 xl:px-0 2xl:pt-48 2xl:pb-28 2xl:pr-0 xl:ml-10">
             <div
-              class="w-full h-full px-4 py-40 bg-center bg-no-repeat md:p-10 lg:p-0 2xl:scale-105"
+              class="w-full h-full flex justify-center lg:justify-start py-44 bg-center bg-no-repeat md:p-10 lg:p-0 2xl:scale-105"
               style={{
                 backgroundImage:
                   "url(https://source.unsplash.com/random/1024x620?keyboard)",
               }}
             >
-              <div class="md:ml-20 lg:py-32 lg:-ml-20 xl:-ml-0 xl:pl-20 xl:py-72 2xl:pl-48">
+              <div class="lg:py-32 lg:-ml-20 xl:-ml-0 xl:pl-20 xl:py-40 2xl:pl-48">
                 <div class="w-[345px] h-[238px] md:w-[470px] md:h-[325px] bg-gray-200 p-6 md:p-10">
                   <img
                     src="https://source.unsplash.com/random/409x257?mousepad"
@@ -80,15 +78,15 @@ const Hero = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div class="py-24 overflow-hidden md:py-32 md:px-10 lg:py-32 lg:pl-32 lg:pr-24 xl:py-0 xl:px-0 2xl:pr-0 xl:ml-10">
+          <div class="py-24 overflow-hidden md:pt-32 md:pb-20 md:px-10 lg:pb-16 lg:pl-32 lg:pr-24 xl:pt-38 xl:px-0 2xl:pt-48 2xl:pb-28 2xl:pr-0 xl:ml-10">
             <div
-              class="w-full h-full px-4 py-40 bg-center bg-no-repeat md:p-10 lg:p-0 2xl:scale-105"
+              class="w-full h-full flex justify-center lg:justify-start py-44 bg-center bg-no-repeat md:p-10 lg:p-0 2xl:scale-105"
               style={{
                 backgroundImage:
                   "url(https://source.unsplash.com/random/1024x620?motherboard)",
               }}
             >
-              <div class="md:ml-20 lg:py-32 lg:-ml-20 xl:-ml-0 xl:pl-20 xl:py-72 2xl:pl-48">
+              <div class="lg:py-32 lg:-ml-20 xl:-ml-0 xl:pl-20 xl:py-40 2xl:pl-48">
                 <div class="w-[345px] h-[238px] md:w-[470px] md:h-[325px] bg-gray-200 p-6 md:p-10">
                   <img
                     src="https://source.unsplash.com/random/409x257?processor"
