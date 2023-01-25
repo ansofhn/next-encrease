70b0336be3e8c7c942fd82f3a90d9c8b3b3c48b1
Ansof Habibunnadjar <ansofhn5@gmail.com>
2023-01-25 11:55:42 +0700

    fix: font size for promotion

--- components/Promotion.js
+++ components/Promotion.js
@@ -5,7 +5,7 @@ const Promotion = () => {
     <div className="bg-softDark">
       <div className="px-8 py-20 md:px-20 xl:px-32 2xl:px-40 bg-background/70 text-softWhite">
         <div className="grid grid-cols-1 gap-10 space-y-6 lg:gap-40 2xl:gap-60 lg:space-y-0 lg:grid-cols-2">
-          <div className="flex items-center justify-center text-5xl font-medium md:text-6xl md:pr-40 lg:pr-0 lg:text-5xl xl:text-6xl 2xl:text-7xl">
+          <div className="flex items-center justify-center text-[44px] font-medium md:text-6xl md:pr-40 lg:pr-0 lg:text-5xl xl:text-6xl 2xl:text-7xl">
             be the best version of you
           </div>
           <div className="flex flex-col justify-center space-y-8 lg:text-left">
