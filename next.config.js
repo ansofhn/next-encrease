d1eb8214c31a3a42e34d875b4ef8252122d47531
Nabil Lanten <nabilanten3@gmail.com>
2023-02-09 18:48:33 +0700

    fix : next-error

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "49.0.2.250",
+        port: "3002",
+        pathname: "/file",
+      },
+    ],
     domains: ["source.unsplash.com", "49.0.2.250"],
   },
 };
